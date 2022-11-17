885b06aa21a147fb0807bf870bdc5f34be566266
PatProMath <opat718@gmail.com>
2022-11-17 10:44:43 +0100

    Final commit

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,5 @@
-#Ensure it is created in development environment
+# Ensure it is created in development environment
 FLASK_ENV=development
-
+# Enable debug mode.
+FLASK_DEBUG=True
 FLASK_APP=app
--- config.py
+++ config.py
@@ -3,8 +3,7 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
+#Some configuration in .flaskenv and .env files
 
 # Connect to the database
 #Get the value of the environment variables
